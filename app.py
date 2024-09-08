095af6cb5f5826b9f856c47a3d9382752af5a9da
Мария Хмелева <ria2201@mail.com>
2024-09-08 16:05:26 +0700

    вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,5 +3,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title> Хмелёва Мария Сергеевна, лабораторная 1</title/>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy: Хмелёва Мария, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
 
