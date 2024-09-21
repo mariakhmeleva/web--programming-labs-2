8e15d6e1aef743f6ef6e6c14475867ee9969f3ba
Мария Хмелева <ria2201@mail.com>
2024-09-21 16:44:31 +0700

    фикс ошибок

--- app.py
+++ app.py
@@ -177,7 +177,25 @@ def example():
         {'name': 'мандарины', 'price': 95}, 
         {'name': 'манго', 'price': 321},  
     ]
+
+    books = [
+        {'name': 'Евгений Онегин', 'author': 'Алесандр Пушкин','janr': 'роман', 'price': 315, 'str': 224}, 
+        {'name': 'Десять негритят', 'author': 'Агата Кристи', 'janr': 'детектив','price': 410, 'str': 288}, 
+        {'name': 'Холодный дом', 'author': 'Чарльз Диккенс', 'janr': 'роман','price': 670, 'str': 1300},
+        {'name': 'Дракула', 'author': 'Брэм Стокер', 'janr': 'роман','price': 378, 'str': 471},
+        {'name': 'Королевы Нью-Йорка', 'author': 'Е.Л. Шень', 'janr': 'young adult','price': 355, 'str': 268},
+        {'name': 'Влюбленный призрак', 'author': 'Марк Леви', 'janr': 'проза','price': 300, 'str': 211},
+        {'name': 'Эмма', 'author': 'Джейн Остен', 'janr': 'роман','price': 400, 'str': 510},
+        {'name': 'Мадонна в меховом манто', 'author': 'Сабахаттин Али', 'janr': 'роман','price': 298, 'str': 192},
+        {'name': 'Прачечная, стирающая надежды', 'author': 'Ким Чжи Юн', 'janr': 'роман','price': 543, 'str': 253},
+        {'name': 'Под маской, или Сила женщины', 'author': 'Луиза Мэй Олкотт', 'janr': 'роман','price': 373, 'str': 146},
+    ]
+
     return render_template('example.html',name=name,number=number,
-                           group=group,course_number=course_number, 
-                           fruits=fruits)
+                           group=group,course_number=course_number, fruits=fruits,
+                           books=books)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
     
