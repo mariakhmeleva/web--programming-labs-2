ca0b000f34e7754f6b6103c10cf82f2d7f41f23b
Мария Хмелева <ria2201@mail.com>
2024-10-16 15:53:17 +0700

    Вывод взраста и анонима

--- lab3.py
+++ lab3.py
@@ -3,23 +3,25 @@ lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
 def lab():
-    name = request.cookies.get('name')
+    name = request.cookies.get('name', 'аноним')
     name_color = request.cookies.get('name_color')
-    return render_template('lab3/lab3.html', name=name, name_color = name_color)
+    age = request.cookies.get('age', 'неизвестен')
+    return render_template('lab3/lab3.html', name=name, name_color = name_color, age=age)
 
 @lab3.route('/lab3/cookie')
 def cookie():
     resp = make_response(redirect('/lab3/'))
     resp.set_cookie('name','Alex', max_age = 5)
-    resp.set_cookie('age','20')
+    resp.set_cookie('age','неизвестен')
     resp.set_cookie('name_color','red')
+    resp.set_cookie('age')
     return resp
 
 @lab3.route('/lab3/del_cookie')
 def del_cookie():
     resp = make_response(redirect('/lab3/'))
     resp.set_cookie('name','Alex', max_age = 0)
-    resp.set_cookie('age','20')
+    resp.set_cookie('age','неизвестен')
     resp.set_cookie('name_color','black')
     return resp
 
