e761cbcf89e630dd05d5fc9a0aa8a67ff0faa982
Егор Лапшин <ria2201@mail.ru>
2024-10-30 18:05:57 +0700

    Список пользователей

--- lab4.py
+++ lab4.py
@@ -122,6 +122,14 @@ def tree():
 
     return redirect('/lab4/tree')
 
+users = [
+    {'login': 'alex', 'password': '123'},
+    {'login': 'bob', 'password': '555'},
+    {'login': 'ria', 'password': '2222'},
+    {'login': 'jack', 'password': '111'}
+]
+
+
 @lab4.route('/lab4/login', methods = ['GET','POST'])
 def login():
     if request.method == 'GET':
@@ -130,8 +138,10 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if login == 'alex' and password == '123':  
-        return render_template('lab4/login.html', error='Успешная авторизация', authorized=True)
+    for user in users:
+        if login == user['login'] and password == user['password']:  
+            return render_template('lab4/login.html', error='Успешная авторизация', authorized=True)
     
     error = 'Неверные логин и/или пароль'
     return render_template('/lab4/login.html', error=error, authorized=False)
+
