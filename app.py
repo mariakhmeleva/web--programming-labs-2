06f13d38f178115e3e92cffd35b11620ecd5a917
Мария Хмелева <ria2201@mail.ru>
2024-11-14 13:43:59 +0700

    Заготовка к 5-ой лабораторной

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ from lab1 import lab1
 from lab2 import lab2 
 from lab3 import lab3
 from lab4 import lab4
+from lab5 import lab5
 
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
+app.register_blueprint(lab5)
 
 @app.route("/")
 @app.route("/index")
@@ -41,6 +43,8 @@ def menu():
     <a href="/lab3"> Лабораторная работа 3 </a> <br>
 
     <a href="/lab4"> Лабораторная работа 4 </a> <br>
+
+    <a href="/lab5"> Лабораторная работа 5 </a> <br>
 
         <footer>
             &copy: Хмелёва Мария, ФБИ-23, 3 курс, 2024
---
+++ lab5.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template,request, make_response, redirect, session
+lab5 = Blueprint('lab5', __name__)
+
+@lab5.route('/lab5/')
+def lab():
+    username='Анонимус'
+    return render_template('lab5/lab5.html',username=username)
+
