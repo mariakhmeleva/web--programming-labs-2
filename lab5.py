974e792065a3086568a9009d8b11ae1ef46ee09e
Мария Хмелева <ria2201@mail.ru>
2024-11-15 13:30:23 +0700

    Исправление SQL - инъекций

--- lab5.py
+++ lab5.py
@@ -37,14 +37,14 @@ def register():
 
     conn,cur = db_connect() 
 
-    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    cur.execute(f"SELECT login FROM users WHERE login=%s;",(login, ))
     if cur.fetchone():
         db_close(conn,cur)
         return render_template('lab5/register.html', 
                                error = "Такой пользователь уже существует")
     
     password_hash = generate_password_hash(password)
-    cur.execute(f"INSERT INTO users (login,password) VALUES ('{login}','{password_hash}');")
+    cur.execute(f"INSERT INTO users (login,password) VALUES (%s,%s);"(login, password_hash))
     conn.commit()
 
     db_close(conn,cur)
@@ -63,7 +63,7 @@ def login():
 
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    cur.execute(f"SELECT * FROM users WHERE login=%s;",(login, ))
     user = cur.fetchone()
 
     if not user:
@@ -95,8 +95,10 @@ def create():
     cur.execute("SELECT * FROM users WHERE login=%s;",(login, ))
     user_id = cur.fetchone()["id"]
 
-    cur.execute(f"INSERT INTO articles(user_id,title,article_text) \
-        VALUES ({user_id}, '{title}','{article_text}');")
+    cur.execute(
+    "INSERT INTO articles (user_id, title, article_text) VALUES (%s, %s, %s);",
+    (user_id, title, article_text)
+)
     
     db_close(conn,cur)
     return redirect('/lab5')
@@ -110,10 +112,12 @@ def list():
 
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT id FROM users WHERE login='{login}';")
-    user_id=cur.fetchone()["id"]
+    cur.execute("SELECT id FROM users WHERE login = %s;", (login,))
+    user_id = cur.fetchone()["id"]
 
-    cur.execute(f"SELECT * FROM articles WHERE user_id='{user_id}';")
-    articles=cur.fetchall()
+    cur.execute("SELECT * FROM articles WHERE user_id = %s;", (user_id,))
+    articles = cur.fetchall()
     db_close(conn,cur)
     return render_template('lab5/articles.html', articles=articles)
+
+
