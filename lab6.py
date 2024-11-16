481d98fb467c8c4fccbb83ef73026a2836d4a1fb
Мария Хмелева <ria2201@mail.ru>
2024-11-16 19:56:48 +0700

    6

--- lab6.py
+++ lab6.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 lab6 = Blueprint('lab6', __name__)
 
-@lab6.route('/lab6')
+@lab6.route('/lab6/')
 def main():
     return render_template('lab6/lab6.html')
