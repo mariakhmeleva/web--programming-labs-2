0f39a3a1465b3f0a6b82eb0e15ed151ab72f9887
Мария Хмелева <ria2201@mail.ru>
2024-11-21 09:56:53 +0700

    стоимость аренды

--- lab6.py
+++ lab6.py
@@ -4,10 +4,11 @@ lab6 = Blueprint('lab6', __name__)
 
 offices=[]
 for i in range(1,11):
-    offices.append({"number": i, "tenant": ""})
+    offices.append({"number": i, "tenant": "","price":700})
 
 @lab6.route('/lab6/')
 def main():
+    login = session.get('login')
     return render_template('lab6/lab6.html')
 
 @lab6.route('/lab6/json-rpc-api/',methods=['POST'])
