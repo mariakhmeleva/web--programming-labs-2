038b3a60aa4e7abe411c571ab6085198b9ba7867
Мария Хмелева <ria2201@mail.ru>
2024-12-26 10:37:58 +0700

    Заготовка к 3 лабораторной

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 import os
 from dotenv import load_dotenv
 
@@ -53,6 +54,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 @app.route("/")
 @app.route("/index")
@@ -88,6 +90,8 @@ def menu():
     <a href="/lab7"> Лабораторная работа 7 </a> <br>
 
     <a href="/lab8"> Лабораторная работа 8 </a> <br>
+
+    <a href="/lab9"> Лабораторная работа 9 </a> <br>
         <footer>
             &copy: Хмелёва Мария, ФБИ-23, 3 курс, 2024
         </footer>
