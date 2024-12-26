b643f7570f59f997b29fef27df7a2b22eb8274c0
Мария Хмелева <ria2201@mail.ru>
2024-12-26 16:38:50 +0700

    Сохранение в сессии

--- lab9.py
+++ lab9.py
@@ -4,14 +4,19 @@ lab9 = Blueprint('lab9', __name__)
 
 @lab9.route('/lab9/')
 def main():
+    if 'name' in session and 'age' in session and 'gender' in session  and 'preference2' in session:
+        return redirect(url_for('lab9.congratulations'))
+    print(session)
     return render_template('lab9/index.html')
 
 @lab9.route('/lab9/age', methods=['GET', 'POST'])
 def age():
+    session['name'] = request.form.get('name')
     return render_template('lab9/age.html')
 
 @lab9.route('/lab9/gender', methods=['GET', 'POST'])
 def gender():
+    session['age'] = request.form.get('age')
     return render_template('lab9/gender.html')
 
 @lab9.route('/lab9/preference1', methods=['GET', 'POST'])
@@ -28,6 +33,8 @@ def preference2():
     
     if request.method == 'POST':
         session['preference1'] = request.form.get('preference1')
+        preference2 = request.form.get('preference2')
+        session['preference2'] = preference2
         return redirect(url_for('lab9.congratulations'))
     return render_template('lab9/preference2.html', preference1=session.get('preference1'))
 
@@ -47,4 +54,11 @@ def congratulations():
     preference1 = session.get('preference1')
     
     # Отображаем финальную страницу
+    print('КОнц',session)
     return render_template('lab9/congratulations.html', name=name, age=age, gender=gender, preference1=preference1, preference2=preference2)
+
+
+@lab9.route('/lab9/reset',methods=['POST'])
+def reset():
+    session.clear()  # Очищаем сессию
+    return redirect(url_for('lab9.main'))
